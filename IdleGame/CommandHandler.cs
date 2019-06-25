3a3f626912d0252fde5ec45af3179af54e54c3ac
Josh <superdog1695@yahoo.com>
2019-06-24 22:43:29 -0400

    Initial commit

---
+++ IdleGame/CommandHandler.cs
@@ -0,0 +1,42 @@
+using System.Globalization;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace IdleGame
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _commands;
+
+        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        {
+            _commands = commands;
+            _client = client;
+        }
+        
+        public async Task InstallCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), services: null);
+        }
+        
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0;
+            
+            if (!(message.HasCharPrefix('+', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) || message.Author.IsBot)
+                return;
+            
+            var context = new SocketCommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(context: context, argPos: argPos, services: null);
+        }
+    }
+}
---
+++ IdleGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using dotenv.net;
+
+namespace IdleGame
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+        
+        static void Main(string[] arg) => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
+        {
+            if (!File.Exists(".env"))
+            {
+                File.Copy(".env.example", ".env");
+                Console.WriteLine(".env file created. Please configure and restart.");
+                return;
+            }
+            DotEnv.Config(false);
+            
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Info
+            });
+
+            _client.Log += Log;
+            
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async void Shutdown()
+        {
+            await _client.StopAsync();
+        }
+    }
+}
