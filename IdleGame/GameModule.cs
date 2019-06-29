2ec9d3365f1e95b3bf53830a83ed1a03b3c2fee0
Josh <superdog1695@yahoo.com>
2019-06-28 21:11:51 -0400

    Can start as a new player. Doesn't do all necessary database operations yet

--- IdleGame/GameModule.cs
+++ IdleGame/GameModule.cs
@@ -8,5 +8,20 @@ namespace IdleGame
     public class GameModule : ModuleBase<SocketCommandContext>
     {
 
+        [Command("new")]
+        public async Task NewPlayer()
+        {
+            var guildUser = (IGuildUser) Context.User;
+            string user = guildUser.Nickname;
+            int success = Program.AddPlayer(Context.User.Id, user);
+            if (success > 0)
+            {
+                await ReplyAsync($"Player \"{user}\" created successfully. Enjoy your journey!");
+            }
+            else
+            {
+                await ReplyAsync($"There was an error creating the player. Sadface");
+            }
+        }
     }
 }
--- IdleGame/Program.cs
+++ IdleGame/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -7,6 +10,8 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using dotenv.net;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Cms;
 
 
 namespace IdleGame
@@ -16,6 +21,8 @@ namespace IdleGame
         private static DiscordSocketClient _client;
         private CommandHandler _commands;
         private string connStr = "server=localhost;user=root;database=idlegame;port=3306";
+        private static MySqlConnection conn;
+        private static MySqlCommand cmd;
 
         static void Main(string[] arg) => new Program().MainAsync().GetAwaiter().GetResult();
         public async Task MainAsync()
@@ -27,7 +34,18 @@ namespace IdleGame
                 return;
             }
             DotEnv.Config(false);
-            
+
+            conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Environment.Exit(1);
+            }
             
             
             _client = new DiscordSocketClient(new DiscordSocketConfig
@@ -37,7 +55,7 @@ namespace IdleGame
             
             _commands = new CommandHandler(_client, new CommandService());
             await _commands.InstallCommandsAsync();
-
+            
             _client.Log += Log;
 
             await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DISCORD_TOKEN"));
@@ -55,7 +73,25 @@ namespace IdleGame
         public static async Task Shutdown()
         {
             await _client.StopAsync();
+            conn.Close();
             Environment.Exit(0);
+        }
+        
+        // SQL functions
+        public static int AddPlayer(ulong id, string name)
+        {
+            string sql = $"INSERT INTO player (ID, Name) VALUES ('{id}','{name}')";
+            try
+            {
+                cmd = new MySqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return 0;
+            }
         }
     }
 }
