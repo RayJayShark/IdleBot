782872fbb602de2be5dfb4b92ff1081abf4d0631
Joshua Ray <superdog1695@yahoo.com>
2019-10-09 01:32:33 -0400

    Added fields and constructor

--- IdleGame/Poker/PokerPlayer.cs
+++ IdleGame/Poker/PokerPlayer.cs
@@ -2,6 +2,19 @@ namespace IdleGame.Poker
 {
     public class PokerPlayer
     {
-        
+        private ulong id;
+        private string name;
+        private Hand hand;
+
+        public PokerPlayer(ulong id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
     }
 }
