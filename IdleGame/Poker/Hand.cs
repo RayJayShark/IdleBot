1ed6174887f70b5a26123179106a5ccec268b0a2
Joshua Ray <superdog1695@yahoo.com>
2019-10-08 23:29:31 -0400

    Added basic constructors

--- IdleGame/Poker/Hand.cs
+++ IdleGame/Poker/Hand.cs
@@ -1,7 +1,26 @@
+using System;
+
 namespace IdleGame.Poker
 {
     public struct Hand
     {
-        
+        private Card[] _cards;
+
+        public Hand(Card card1, Card card2)
+        {
+            _cards = new Card[] {card1, card2};
+        }
+
+        public Hand(Card[] cards)
+        {
+            if (cards.Length == 2)
+            {
+                _cards = cards;
+            }
+            else
+            {
+                throw new Exception("Hand must contain 2 cards.");
+            }
+        }
     }
 }
