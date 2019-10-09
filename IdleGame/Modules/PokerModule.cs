446e3bf2402b623c17a8400e95424c5ea9ca6057
Joshua Ray <superdog1695@yahoo.com>
2019-10-08 22:49:51 -0400

    Added some empty commands

--- IdleGame/Modules/PokerModule.cs
+++ IdleGame/Modules/PokerModule.cs
@@ -1,10 +1,33 @@
+using System.Threading.Tasks;
 using Discord.Commands;
+using IdleGame.Services;
 
 namespace IdleGame.Modules
 {
     [Name("Poker Commands")]
     public class PokerModule : ModuleBase<SocketCommandContext>
     {
+        private PokerService _pokerservice { get; set; }
+
+        [Command("newgame")]
+        [Alias("new")]
+        public async Task StartNewGame()
+        {
+            //TODO: Start new game, allow to join
+        }
         
+        [Command("joingame")]
+        [Alias("join")]
+        public async Task JoinGame()
+        {
+            //TODO: Join active game
+        }
+
+        [Command("start")]
+        [Alias("begin", "startgame")]
+        public async Task BeginGame()
+        {
+            //TODO: Start playing the new game
+        }
     }
 }
