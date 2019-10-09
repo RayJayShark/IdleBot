1d78ec8a20190c444071e2c5fa32efc0f38ff3c7
Joshua Ray <superdog1695@yahoo.com>
2019-10-08 23:44:22 -0400

    Default constructor initializes a full standard deck

--- IdleGame/Poker/Deck.cs
+++ IdleGame/Poker/Deck.cs
@@ -1,10 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace IdleGame.Poker
 {
     public class Deck
     {
-        
+        public List<Card> _cards = new List<Card>();
+
+        public Deck()
+        {
+            foreach (var c in "cshd")
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    _cards.Add(new Card(c.ToString(), i));
+                }
+            }
+        }
     }
     
 }
