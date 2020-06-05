9cbc12d744a0f44b26d96613c819bc4d8f792cfd
Joshua Ray <superdog1695@yahoo.com>
2020-06-05 00:15:45 -0400

    Made SqlService variable private vs protected

--- IdleGame/Modules/PlanetModule.cs
+++ IdleGame/Modules/PlanetModule.cs
@@ -6,7 +6,7 @@ namespace IdleGame.Modules
     [Name("Planet and Party Commands")]
     public class PlanetModule : ModuleBase<SocketCommandContext>
     {
-        protected readonly SqlService _sqlService;
+        private readonly SqlService _sqlService;
         
         public PlanetModule(SqlService sqlService = null)
         {
