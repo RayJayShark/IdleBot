007c9ea9289f9e2c0c4dba9bf486d0149ec8078f
Joshua Ray <superdog1695@yahoo.com>
2020-06-09 01:14:31 -0400

    Added a list of DM channels for each player added to the party

--- IdleGame/Party.cs
+++ IdleGame/Party.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Discord;
+using Discord.WebSocket;
 using IdleGame.Classes;
 
 namespace IdleGame
@@ -6,18 +8,24 @@ namespace IdleGame
     public class Party
     {
         private readonly int _id;
+        private readonly DiscordSocketClient _client;
         private readonly List<Player> _playerList;
+        private readonly List<IDMChannel> _dmList;
 
-        public Party(int id)
+        public Party(int id, DiscordSocketClient client)
         {
             _id = id;
+            _client = client;
             _playerList = new List<Player>();
+            _dmList = new List<IDMChannel>();
         }
 
-        public Party(int id, Player player)
+        public Party(int id, Player player, DiscordSocketClient client)
         {
             _id = id;
+            _client = client;
             _playerList = new List<Player> {player};
+            _dmList = new List<IDMChannel> { _client.GetUser(player.GetId()).GetOrCreateDMChannelAsync().Result };
             player.SetParty(id);
         }
 
@@ -42,14 +50,19 @@ namespace IdleGame
                 return false;
             
             _playerList.Add(player);
+            _dmList.Add(_client.GetUser(player.GetId()).GetOrCreateDMChannelAsync().Result);
             player.SetParty(_id);
             return true;
         }
 
         public void RemovePlayer(Player player)
         {
-            if (_playerList.Remove(player))
-                player.SetParty(-1);
+            var index = _playerList.FindIndex(p => p.GetId() == player.GetId());
+            _playerList.RemoveAt(index);
+            _dmList.RemoveAt(index);
+            player.SetParty(-1);
+            
+        }
         }
     }
 }
